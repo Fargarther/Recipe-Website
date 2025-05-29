cf7ba2af7273f793b454513fda1bd1e1730de4f9
Fargarther <bensoaj2@gmail.com>
2025-05-29 17:21:32 -0500

    Ree

---
+++ src/components/Home/Spotlight/decorations/index.jsx
@@ -0,0 +1,3 @@
+// src/components/Home/Spotlight/decorations/index.js
+export { default as Stickers, SeasonalStickers } from './Stickers';
+export { default as PostItNotes, postItMessages } from './PostItNotes';
